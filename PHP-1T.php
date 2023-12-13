d41d2dd1a5a7d5537b007a0b611fc91ece78259e
Yarmt <shminke.yaroslav@yandex.ru>
2023-12-14 00:20:51 +0300

    изменение 1

--- PHP-1T.php
+++ PHP-1T.php
@@ -3,8 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <title>Веб-разработчик Ярослав</title>
-  <link rel="stylesheet" href="Styles/main.css">
+  <link rel="stylesheet" href="1T.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  
 </head>
 <body>
   <header>
@@ -22,14 +23,29 @@
     <table>
       <thead>
         <tr>
-          <th>Название оборудования</th>
-          <th>Цена</th>
+          <?php 
+          $naming = "Название оборудования";
+          echo '<th>'.  $naming; '</th>'
+          ?>
+          <?php 
+          $price = "Цена";
+          echo '<th>'.  $price; '</th>'
+          ?>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>Центральный процессор</td>
           <td><?php echo 33000; ?></td>
+          <!-- <?php //тег для php страниц  ?> -->
+          <!-- <?php //echo = output value  ?> -->
+          <!-- <?php //$ = value variable  ?> -->
+          <!-- <?php // $_GET['video'] = Global variables ?> -->
+          <!-- <?php // $_add = functions($a,$b)  ?> -->
+          <!-- <?php // return $a*$b = first-class functions   ?> -->
+          <!-- <?php // return $a*$b = first-class functions   ?> -->
+          <!-- <?php // array_map(function ($a,$b){ return $a*$b; }) = anonymous functions   ?> -->
+
         </tr>
         <tr>
           <td>Оперативная память</td>
@@ -39,13 +55,14 @@
     </table>
   </main>
   <section id="section1">
-    <a href="https://httpbin.org/post">Ссылка на раздел с котиками</a>
+    <a href="https://httpbin.org/post">Ссылка на раздел с доп.информацией</a>
   </section>
   
-  <footer>
+  <footer >
     <p>Контактная информация</p>
     <p>© <?php echo date("Y"); ?> Шминке Я.Д. Московский Политех. Все права защищены.</p>
   </footer>
   
 </body>
 </html>
+
